3f5846b4e11c8d926df5d5ca3b03f420192907a2
Asliddin <asliddinrashidovich7@gmail.com>
2025-09-10 13:14:03 +0500

    dunder methods

--- polymorfise.py
+++ polymorfise.py
@@ -28,8 +28,15 @@ class Talaba(Shaxs):
         """Talabaning xususiyatlari"""
         super().__init__(ism, familiya, passport, tyil, pul)
         self.fanlar=[]
+        self.pul = pul
         Talaba.univer += 1
 
+    def __repr__(self):
+        return 'this is the mirshodning clasi'
+
+    def __eq__(self, value):
+        return self.pul == value.pul
+    
     @classmethod
     def get_talabalar_soni(cls):
         return cls.talabalar_soni
@@ -50,8 +57,20 @@ class Fan:
         self.uquvchi = uquvchi
         self.nom = nom 
         self.yil = yil
+        self.students = []
     
+    def __setitem__(self, index, name):
+        self.students[index] = name
 
+    def __getitem__(self, index):
+        self.students[index]
+
+    def add_fan(self, *qiymat):
+        for sub in qiymat:
+            if isinstance(sub, Talaba):
+                self.students.append(sub)
+            else:
+                print('iltimow kiriting')
 class Professor(Shaxs):
     def __init__(self, ism, familiya, passport, tyil):
         super().__init__(ism, familiya, passport, tyil)
@@ -69,18 +88,28 @@ class Admin(Professor):
         return f"{self.ism} foydalanuvchi bloklandi"
     
 matem = Fan('matematika', 'asliddin', 2006)
+english = Fan('english', 'asliddin', 2006)
+
+
 mirshod = Talaba('Mirshod', 'Normurodov', 'AD234234', 2006, 111)
-mirshod2 = Talaba('Mirshod', 'Normurodov', 'AD234234', 2006, 111)
+mirshod2 = Talaba('Mirshod2', 'Normurodov', 'AD234234', 2006, 1111)
+mirshod3 = Talaba('Mirshod3', 'Normurodov', 'AD234234', 2006, 1111)
+
+matem.add_fan(mirshod, mirshod2, mirshod3)
+matem.__setitem__(0, mirshod)
+matem.__setitem__(1, mirshod2)
+print(matem.__getitem__(1))
+
 # sharof = Professor('Sharofiddin', 'Normurodov', 12323131, 1993)
 # adminka = Admin('shahzoda', 'Normuhammedova', 12313232, 2008)
 
-asliddin = Shaxs('asliddin', 'norboyev', 12333231, 2006, 1000)
+# asliddin = Shaxs('asliddin', 'norboyev', 12333231, 2006, 1000)
 
-mening_pulim = asliddin.get_pul()
+# mening_pulim = asliddin.get_pul()
 
 # print(asliddin.__pul)
-print(mening_pulim)
-print(mirshod.get_talabalar_soni())
+# print(mening_pulim)
+# print(mirshod.get_talabalar_soni())
 # print(matem.nom)
 # # mirshod.fanga_yozil(matem)
 # mirshod.remove_fan(matem)
